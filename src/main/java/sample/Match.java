adf15ad6c426a56dfd43bb9a07f1053a99cc8bef
Krzysztof <reviewalfa1@gmail.com>
2017-09-19 19:49:27 +0200

    fsdfsddsf

---
+++ src/main/java/sample/Match.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by RENT on 2017-09-19.
+ */
+//3. (2) Stwórz encję Match (player1Score, player2Score, finished, round, occurenceDateTime, String player1, String player2)
+public class Match {
+    int player1Score;
+    int player2Score;
+    boolean finished;
+    int round;
+    LocalDateTime occurenceDateTime;
+    String player1;
+    String player2;
+}
