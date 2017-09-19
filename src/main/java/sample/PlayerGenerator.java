acda7c672058dd6eda68d3a73f8c6ebe697517e8
Grzegorz Budnik <budnikgrzegorz@interia.pl>
2017-09-19 20:15:56 +0200

    Utworzenie PlayerGenerator

--- src/main/java/sample/PlayerGenerator.java
+++ src/main/java/sample/PlayerGenerator.java
@@ -1,17 +1,23 @@
 package sample;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 /**
  * Created by RENT on 2017-09-19.
  */
 public class PlayerGenerator {
+    SessionFactory sessionFactory = SessionFactoryManager.getSessionFactory();
+    Session session = sessionFactory.openSession();
 
-    public Player generate(int numeberOfPlayers) {
+    public void generate(int numeberOfPlayers) {
         for (int i = 0; i < numeberOfPlayers; i++) {
             Player player = new Player();
-            player.setUsername(String.valueOf(i));
+            player.setUsername("Gracz" + String.valueOf(i));
+            session.save(player);
 
         }
-
-        return null;
+        session.getTransaction().commit();
+        sessionFactory.close();
     }
-}
+} 
