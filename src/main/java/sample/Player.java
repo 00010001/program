479d68ca9fa5adf9ca0ef11688e7cfd04bdf45ec
Grzegorz Budnik <budnikgrzegorz@interia.pl>
2017-09-19 19:51:01 +0200

    Pełny player

--- src/main/java/sample/Player.java
+++ src/main/java/sample/Player.java
@@ -1,8 +1,13 @@
 package sample;
 
+import javax.persistence.Entity;
+
 /**
  * Created by RENT on 2017-09-19.
  */
-
+@Entity
 public class Player {
+    private String firstName;
+    private String lastName;
+    private String username;
 }
